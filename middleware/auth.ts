4c03b971f56298142b3d5ceb4a091787a1ccbd64
arieltecay <arieltecay@gmail.com>
2026-08-22 11:33:58 -0300

    fix: fix lint error - ES2015 module syntax for Express namespace

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -14,6 +14,7 @@ export interface AuthPayload {
 }
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       user?: AuthPayload;
